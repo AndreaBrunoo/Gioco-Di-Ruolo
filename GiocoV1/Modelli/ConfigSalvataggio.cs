48c4bd2abf77b46ca95764ff0007ee4e95bf6033
AndreaBrunoo <andrea.bruno.06.ge@gmail.com>
2026-05-25 18:23:09 +0200

    salvataggio

---
+++ GiocoV1/Modelli/ConfigSalvataggio.cs
@@ -0,0 +1,7 @@
+namespace GiocoV1.Modelli;
+public static class ConfigSalvataggi
+{
+    public const string CartellaSalvataggi = "Salvataggi";
+    public const string Prefisso = "save_";
+    public const string Estensione = ".json";
+}
